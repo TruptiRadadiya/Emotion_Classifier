8a111a3ee14e98606b415ed0a8d5b7ac83779bf7
TruptiRadadiya <radadiyatrupti21@gmail.com>
2024-08-25 14:35:12 +0800

    Add notebooks and models

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-#https://www.gradio.app/guides/image-classification-in-tensorflow
+# #https://www.gradio.app/guides/image-classification-in-tensorflow
 
 import gradio as gr
 import tensorflow as tf
 import numpy as np
 
 # Load trained model
-model = tf.keras.models.load_model('imageclassifier.h5')
+model = tf.keras.models.load_model('Models/EmotionClassifier.h5')
 
 # Define a function to predict the class
 def predict_class(image):
 
     # Preprocess the image (resize and normalize)
-    resize = tf.image.resize(image,(256,256))
+    resize = tf.image.resize(image, (150, 150))  # Resize to the input size of the model
+    resize = np.array(resize) / 255.0  # Normalize the image to [0,1]
 
     # Make predictions
-    yhat = model.predict(np.expand_dims(resize/255,0))
+    yhat = model.predict(np.expand_dims(resize, 0))  # Predict using the model
 
-    # Debugging: Print the prediction value
     print(yhat)
 
     # Determine the class based on the threshold
@@ -29,4 +29,4 @@ def predict_class(image):
 image_input = gr.Image(type="pil")
 output_text = gr.Textbox(label="Predicted Emotion")
 
-gr.Interface(fn=predict_class, inputs=image_input, outputs=output_text, title="Group 1 Project 4 Emotion Classifier").launch(share=True)
+gr.Interface(fn=predict_class, inputs=image_input, outputs=output_text, title="Emotion Classifier").launch(share=True)
